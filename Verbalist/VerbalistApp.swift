700a5f4a5fc49090bb3bfda1caf24d28eec06a92
maschad96 <matthew.schad@consensys.net>
2025-06-01 01:29:25 -0700

    Initial Commit

---
+++ Verbalist/VerbalistApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbalistApp.swift
+//  Verbalist
+//
+//  Created by Matt Schad on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct VerbalistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VerbalistTests/VerbalistTests.swift
@@ -0,0 +1,17 @@
+//
+//  VerbalistTests.swift
+//  VerbalistTests
+//
+//  Created by Matt Schad on 6/1/25.
+//
+
+import Testing
+@testable import Verbalist
+
+struct VerbalistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
